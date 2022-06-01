9e5c21acddc9c0c1cf099a5c99ecee4eace9c6cd
purnsahithi <purnasahithimuppidi@gmail.com>
2022-06-01 23:33:15 +0530

    Changed new headings

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -3,7 +3,7 @@ import React from "react";
 function Todolist(props) {
     return (
         <div>
-            <h2 className="display-5 text-primary" > list of todo</h2 >
+            <p className="display-5 text-primary" > list of todo</p>
             <div>
                 {
                     props.todolist.map((todo, index) => <h1 key={index}>{todo}</h1>)
